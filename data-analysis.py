04c96a0eed9cea2598f16fc8eeca993cc1d6c8c1
recabet <rajab.iskenderli@gmail.com>
2024-07-11 18:17:47 +0400

    Needs some additional functionality

--- data-analysis.py
+++ data-analysis.py
@@ -1,13 +1,40 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from logisticReg import LogisticRegression
-from sklearn.model_selection import train_test_split
+from logisticReg import my_LogisticRegression
 
-df=pd.read_csv("exams.csv")
-df_passed=df[df["admitted"]==1]
-df_failed=df[df["admitted"]==0]
+df = pd.read_csv("exams.csv")
+df = df.sample(len(df)).copy()
+df_passed = df[df["admitted"] == 1]
+df_failed = df[df["admitted"] == 0]
+df.head()
 
-plt.scatter(df_passed["exam_1"],df_passed["exam_2"],color='b')
-plt.scatter(df_failed["exam_1"],df_failed["exam_2"],color='r')
+plt.scatter(df_passed["exam_1"], df_passed["exam_2"], color='g')
+plt.scatter(df_failed["exam_1"], df_failed["exam_2"], color='r')
+
+
 plt.show()
+
+df2 = df.copy()
+
+weights = np.random.rand(df.shape[1])
+y = df["admitted"]
+y_train = df2["admitted"][:90]
+y_test = df2["admitted"][90:]
+df_train = df2[:90].copy()
+df_test = df2[90:].copy()
+df_train["exam_1"] = (df_train["exam_1"] - df_train["exam_1"].min()) / (
+		df_train["exam_1"].max() - df_train["exam_1"].min())
+df_train["exam_2"] = (df_train["exam_2"] - df_train["exam_2"].min()) / (
+		df_train["exam_2"].max() - df_train["exam_2"].min())
+df_test["exam_1"] = (df_test["exam_1"] - df2[:90]["exam_1"].min()) / (
+		df2[:90]["exam_1"].max() - df2[:90]["exam_1"].min())
+df_test["exam_2"] = (df_test["exam_2"] - df2[:90]["exam_2"].min()) / (
+		df2[:90]["exam_2"].max() - df2[:90]["exam_2"].min())
+inputs = np.array([np.ones(len(df_train)), df_train["exam_1"], df_train["exam_2"]]).T
+
+model=my_LogisticRegression()
+w = model.fit(df_train, y_train)
+
+print(y_test)
+print(model.predict(df_test))
--- logisticReg.py
+++ logisticReg.py
@@ -1,34 +1,31 @@
 import numpy as np
-
-
 def sigmoid (x):
 	return 1 / (1 + np.exp(-x))
 
 
-def costFunction (y, h):
-	return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
-
-
-class LogisticRegression:
-	def __init__ (self, alpha=0.01, iterations=1000):
+class my_LogisticRegression:
+	def __init__ (self, alpha=0.01, iterations=10000):
 		self.alpha = alpha
 		self.iterations = iterations
 		self.theta = None
 	
-	def predict (self, X, theta):
-		p = sigmoid(np.dot(X, theta))
+	def cost_function (self, y, h):
+		return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+	
+	def fit (self, X, y, cost=False):
+		self.theta = np.zeros(X.shape[1])
+		for i in range(self.iterations):
+			z = np.dot(X, self.theta)
+			h = sigmoid(z)
+			g = np.dot(X.T, (h - y)) / len(y)
+			self.theta -= self.alpha * g
+			if cost and i % 1000 == 0:
+				print(f"Iteration: {i}, Cost: {self.cost_function(y, h)}")
+		
+		return self.theta
+	
+	def predict (self, X):
+		p = sigmoid(np.dot(X, self.theta))
 		p[p >= 0.5] = 1
 		p[p < 0.5] = 0
 		return p
-	
-	def fit (self, X, y, theta, alpha=0.1, iterations=10000, cost=False):
-		costH = []
-		for i in range(iterations):
-			z = np.dot(X, theta)
-			h = sigmoid(z)
-			g = np.dot(X.T, (h - y)) / len(y)
-			theta -= alpha * g
-			costH.append(costFunction(y, h))
-			if cost:
-				print(i, costH[i])
-		return theta
